699937b1673ace10f3bacb6e55bdca2e3a135e92
alissonRH <alisson.honorato@live.com>
2022-05-09 18:38:32 -0300

    terminei

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 export default class SolarSystem extends React.Component {
   render() {
-    return (<div data-testid="solar-system"><Title /></div>);
+    return (
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+        <div>
+          {planets.map((planet) => (
+            <PlanetCard
+              key={ planet.name }
+              planetName={ planet.name }
+              planetImage={ planet.image }
+            />
+          ))}
+        </div>
+      </div>
+    );
   }
 }
