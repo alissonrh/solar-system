bbc23ac3ffde6bd985b820ef840cb276bbc998a5
alissonRH <alisson.honorato@live.com>
2022-05-09 16:30:34 -0300

    até a 4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <>
+        <Header />
+        <SolarSystem />
+        <Title headline="Planetas" />
+      </>
+    );
   }
 }
 
---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Title from './Title';
+
+export default class SolarSystem extends React.Component {
+  render() {
+    return (<div data-testid="solar-system"><Title /></div>);
+  }
+}
